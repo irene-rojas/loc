4635426f84b2c2fdd1f3cb916538e009f14f3d8d
Irene Rojas <irene@smallwondermedia.com>
2019-09-11 21:53:39 -0400

    basic tests no search yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`https://www.loc.gov/search/?fo=json&q=baseball`)
+        .then(res => {
+            setResults(res.data.results[0].description[0]);
+            // console.log(res.data);
+            // console.log(res.data.facet_trail[0].value);
+            // console.log(res.data.results);
+            console.log(res.data.results[0].description[0]);
+        });
+    }, []);
+
+
+
   return (
     <div className="App">
 
         <header>Library of Congress</header>
+
+        <div>
+            {/* <img src="http://www.loc.gov/static/exhibitions/baseball-americana/155_155.jpg"/> */}
+            <p>{results}</p>
+
+        </div>
 
     </div>
   );
