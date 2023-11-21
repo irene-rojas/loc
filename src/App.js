5bdeb592d36606b4c248ef7e6a61b714b1f23283
Irene Rojas <irene@smallwondermedia.com>
2023-11-21 16:54:36 -0500

    results div styling

--- src/App.js
+++ src/App.js
@@ -20,81 +20,85 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="parallax">
 
-        <div className="header">
-            <h1>Search the Library of Congress</h1>
+        <div className="App">
 
-            <p>Powered by the <a href="https://libraryofcongress.github.io/data-exploration/" target="_blank" rel="noopener noreferrer">LOC API</a><br />
-            No-frills portal to search the Library of Congress's collection.<br />
-            Returns first 150 results, when available.<br />
-            Search by either a specific format OR all formats.</p>
-        </div>
+            <div className="header">
+                <h1>Search the Library of Congress</h1>
 
-        <div className="searchFormDiv">
-            <form 
-                className="searchForm"
-                onSubmit={event => {
-                    event.preventDefault();
-                    imageSearch();
-                }}>
+                <p>Powered by the <a href="https://libraryofcongress.github.io/data-exploration/" target="_blank" rel="noopener noreferrer">LOC API</a><br />
+                No-frills portal to search the Library of Congress's collection.<br />
+                Returns first 150 results, when available.<br />
+                Search by either a specific format OR all formats.</p>
+            </div>
 
-                <h2>Enter a search term:</h2>
-                <input 
-                    className="searchTerm"
-                    title="Enter a search term"
-                    value={query}
-                    onChange={event => {
+            <div className="searchFormDiv">
+                <form 
+                    className="searchForm"
+                    onSubmit={event => {
                         event.preventDefault();
-                        setQuery(event.target.value);
-                    }}
-                />
-                <br/>
-
-                <h2>Option 1: Select media format</h2>
-                <select
-                    title="Select media format"
-                    className="formatDropdown"
-                    value={format}
-                    onChange={event => {
-                        event.preventDefault();
-                        setFormat(event.target.value);
+                        imageSearch();
                     }}>
-                    <option>Select media format</option>
-                    <option value="photos">Photos</option>
-                    <option value="audio">Audio</option>
-                    <option value="film-and-videos">Film/Videos</option>
-                    <option value="newspapers">Newspapers</option>
-                    <option value="maps">Maps</option>
-                    <option value="manuscripts">Manuscripts</option>
-                    <option value="websites">Websites</option>
-                    <option value="notated-music">Printed Music</option>
-                </select>
-                <br/>
-                <button>Submit</button>
-            </form>
+
+                    <h2>Enter a search term:</h2>
+                    <input 
+                        className="searchTerm"
+                        title="Enter a search term"
+                        value={query}
+                        onChange={event => {
+                            event.preventDefault();
+                            setQuery(event.target.value);
+                        }}
+                    />
+                    <br/>
+
+                    <h2>Select media format</h2>
+                    <select
+                        title="Select media format"
+                        className="formatDropdown"
+                        value={format}
+                        onChange={event => {
+                            event.preventDefault();
+                            setFormat(event.target.value);
+                        }}>
+                        <option>Select media format</option>
+                        <option value="photos">Photos</option>
+                        <option value="audio">Audio</option>
+                        <option value="film-and-videos">Film/Videos</option>
+                        <option value="newspapers">Newspapers</option>
+                        <option value="maps">Maps</option>
+                        <option value="manuscripts">Manuscripts</option>
+                        <option value="websites">Websites</option>
+                        <option value="notated-music">Printed Music</option>
+                    </select>
+                    <br/>
+                    <button>Submit</button>
+                </form>
+            </div>
+
+            <br/>
+
+            <div className="resultsDiv">
+                {results.map((result, index) => {
+                    return (
+                            <Results
+                                className="singleResult" 
+                                key={index}
+                                title={result.title}
+                                description={result.description}
+                                image={result.image_url[0]}
+                                url={result.url}
+                            />
+                    )
+                })}
+            </div>
+
         </div>
-
-        <br/>
-
-        <div className="resultsDiv">
-            {results.map((result, index) => {
-                return (
-                        <Results
-                            className="singleResult" 
-                            key={index}
-                            title={result.title}
-                            description={result.description}
-                            image={result.image_url[0]}
-                            url={result.url}
-                        />
-                )
-            })}
-        </div>
-
+        {/* end App */}
 
     </div>
-    // end App
+    // end parallax
   );
 }
 
