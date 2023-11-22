14a7ae820bc7daf00ade0a10804fd21b7955b96e
Irene Rojas <irene@smallwondermedia.com>
2023-11-21 20:18:00 -0500

    Updates

---
+++ precache-manifest.ebe38c5acf9086acf62b330d2acc6a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051f7033021cf488967196e9e0f1f15f",
+    "url": "/loc/index.html"
+  },
+  {
+    "revision": "72b256533516ad693630",
+    "url": "/loc/static/css/main.2b429c01.chunk.css"
+  },
+  {
+    "revision": "a8b90d7d2146a1bf3f9f",
+    "url": "/loc/static/js/2.c09f8e1c.chunk.js"
+  },
+  {
+    "revision": "72b256533516ad693630",
+    "url": "/loc/static/js/main.006b2e96.chunk.js"
+  },
+  {
+    "revision": "bac6530e1db2011f5b4c",
+    "url": "/loc/static/js/runtime~main.83dfb5c0.js"
+  }
+]);
